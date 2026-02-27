0ee9d0c7d210b2c68bf699873a74b08ac27eecb7
maxy_36 <maxi.morales36@gmail.com>
2026-02-27 09:53:33 -0300

    playerMove

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,60 @@
+using System.Xml.Schema;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
+    public PlayerInput playerInput;
+
+    public float speed;
+    public int facingDirection = 1; 
+
+
+    public Vector2 moveInput;
+
+    // .........
+
+    void Update()
+    {
+        Flip();    
+    }
+
+
+
+    void FixedUpdate()
+    {
+        float targetSpeed = moveInput.x * speed;
+        rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);   
+    }
+
+
+
+
+    void Flip() 
+    {
+        if (moveInput.x > 0.1f) 
+        {
+            facingDirection = 1;
+        }
+        else if(moveInput.x < -0.1f)
+        {
+            facingDirection= -1;
+        }
+
+     transform.localScale = new Vector3(facingDirection, 1, 1);
+    }
+
+
+
+
+
+
+
+
+    public void OnMove (InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
 }
+ 
